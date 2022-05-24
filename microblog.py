7926593c98a9895d9131ff26f9bf5ed449983654
Austin Kliebhan <akliebhan@daapllc.com>
2022-05-23 23:06:48 -0500

    Migrating over to version controlled project.

---
+++ microblog.py
@@ -0,0 +1,7 @@
+from app import app, db
+from app.models import User, Post
+# The flask application instance is called app and is a member of the app package.
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
