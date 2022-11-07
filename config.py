0634917cc6ebb4293560d687b988dadbd4a0e12b
Austin Kliebhan <akliebhan@daapllc.com>
2022-11-06 20:46:45 -0600

    Chapter 16 adds.

--- config.py
+++ config.py
@@ -19,3 +19,4 @@ class Config(object):
     LANGUAGES = ["en", "es"]
     POSTS_PER_PAGE = 25
     # MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
